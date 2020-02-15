0532c64f8aefbd7119afa7b6ab9cc241fdd87cac
plocket <52798256+plocket@users.noreply.github.com>
2020-02-15 09:02:51 -0500

    Tests tokenType matching tests, moves function
    Some day we'll add jest. For now, we'll just wallow in misery.

--- multimode.js
+++ multimode.js
@@ -547,6 +547,10 @@ const seekInnerMode = function ({ stream, state, tokenTypes, openers }) {
 //   }
 // };  // Ends stringToWordBreakRegex()
 
+const doesTestExist = function ( test ) {
+  return test !== null && test !== undefined;
+};
+
 const didMatchTokenType = function ( tokenTypes, tokenTypeMatcher ) {
   // What can `tokenTypes` be?
   // Just an array? Of strings? Of regexps? A string
@@ -570,10 +574,52 @@ const didMatchTokenType = function ( tokenTypes, tokenTypeMatcher ) {
     matcherRegex = new RegExp( `\\b${ escapeRegExp( tokenTypeMatcher )}\\b` );
   }
 
+
   let passesTest = matcherRegex.test( tokenTypes );
+  
   console.log( passesTest, tokenTypes );
   return passesTest;
 };  // Ends didMatchTokenType()
+
+// Guide for future jest
+let test_didMatchTokenType = function () {
+  // let expected  = [ true, false, true, false, true, false, true, false, true, true ];
+  let expected  = [];
+  let actual    = [];
+  let test0 = [ 'meta error', 'meta', didMatchTokenType( 'meta error', 'meta' )];
+  expected.push( true ); actual.push([ expected[ 0 ] === test0[ 2 ], test0 ]);
+  let test1 = [ 'atom error', 'meta', didMatchTokenType( 'atom error', 'meta' )];
+  expected.push( false ); actual.push([ expected[ 1 ] === test1[ 2 ], test1 ]);
+  let test2 = [ 'meta error', /meta/, didMatchTokenType( 'meta error', /meta/ )];
+  expected.push( true ); actual.push([ expected[ 2 ] === test2[ 2 ], test2 ]);
+  let test3 = [ 'atom error', /meta/, didMatchTokenType( 'atom error', /meta/ )];
+  expected.push( false ); actual.push([ expected[ 3 ] === test3[ 2 ], test3 ]);
+  let test4 = [ 'meta error', /\bmeta\b/, didMatchTokenType( 'meta error', /\bmeta\b/ )];
+  expected.push( true ); actual.push([ expected[ 4 ] === test4[ 2 ], test4 ]);
+  let test5 = [ 'atom error', /\bmeta\b/, didMatchTokenType( 'atom error', /\bmeta\b/ )];
+  expected.push( false ); actual.push([ expected[ 5 ] === test5[ 2 ], test5 ]);
+  let funcTrue  = function(){ return true; };
+  let test6 = [ 'atom error', 'funcTrue', didMatchTokenType( 'atom error', funcTrue )];
+  expected.push( true ); actual.push([ expected[ 6 ] === test6[ 2 ], test6 ]);
+  let funcFalse = function(){ return false; };
+  let test7 = [ 'atom error', 'funcFalse', didMatchTokenType( 'atom error', funcFalse )];
+  expected.push( false ); actual.push([ expected[ 7 ] === test7[ 2 ], test7 ]);
+  let test8 = [ 'atom error', null, didMatchTokenType( 'atom error', null )];
+  expected.push( true ); actual.push([ expected[ 8 ] === test8[ 2 ], test8 ]);
+  let test9 = [ 'atom error', 'undefined', didMatchTokenType( 'atom error' )];
+  expected.push( true ); actual.push([ expected[ 9 ] === test9[ 2 ], test9 ]);
+
+  let allPassed = true;
+  for ( let index = 0; index < actual.length; index++ ) {
+    if ( !actual[ index ][ 0 ]) {
+      allPassed = false;
+      console.log( 'test_didMatchTokenType test', index, 'failed. expected:', expected[ index ], 'but got:', actual[ index ] );
+    }
+  }
+
+  return 'test_didMatchTokenType allPassed:' + allPassed;
+};
+test_didMatchTokenType();
 
 // const didMatchWholeLine = function ( tokenTypes, wholeLine, tests ) {
 
@@ -582,10 +628,5 @@ const didMatchTokenType = function ( tokenTypes, tokenTypeMatcher ) {
 // const didMatchCurrString = function ( tokenTypes, currString, tests ) {
 
 // };  // Ends didMatchCurrString()
-
-
-const doesTestExist = function ( test ) {
-  return test !== null && test !== undefined;
-};
 
 });
